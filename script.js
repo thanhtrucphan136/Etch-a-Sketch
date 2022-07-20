9006e4534455354f1db3058065dfa4095c1d69a7
Truc Phan <trucphan@my.unt.edu>
2022-07-19 20:53:44 -0500

    add function to color when hovering and button to reset the whole board and button to delete each grid

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let container = document.getElementById('container');
+let color = 'black';
 
 function drawingBoard(size){
-    
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     
     for (let i = 0; i < size*size; i++){
         let newGrid = document.createElement('div');
-        //newGrid.style.background = 'yellow';
+        newGrid.addEventListener('mouseover', coloring);
+        newGrid.style.background = 'yellow';
         newGrid.classList.add('grid');
         container.appendChild(newGrid);
     }
@@ -18,9 +19,22 @@ let sizeBtn = document.querySelector('.size-btn');
 sizeBtn.addEventListener('click', changeSize);
 
 function changeSize(){
-    let grids = container.querySelectorAll('div');
+    let grids = container.querySelectorAll('.grid');
     grids.forEach((e) => e.remove());
     let boardSize = document.getElementById('size-input').value;
     drawingBoard(boardSize);
     console.log('clicked');
 }
+function coloring(){
+    this.style.background = color;
+}
+
+function changeColor(colorChoice){
+    color = colorChoice;
+}
+
+
+
+
+
+
